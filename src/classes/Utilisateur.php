f74caf58f8ac55a2107d7739d71e183fbd04eefd
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-01 12:08:19 +0100

    définissez les attributs de la classe utilisateur

---
+++ src/classes/Utilisateur.php
@@ -0,0 +1,10 @@
+<?php
+class Utilisateur{
+    protected ?int $id;
+    protected ?String $email;
+    protected ?String $nom;
+    protected ?String $prenom;
+    protected ?String $password;
+    protected ?String $role;
+}
+?>
