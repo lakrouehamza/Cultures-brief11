621811672400c5a3a286647022bbbfd554ce2bd4
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-01 12:16:26 +0100

    ajoutez le constructeur  de class Utilisateur avec des arguments par défaut

--- src/classes/Utilisateur.php
+++ src/classes/Utilisateur.php
@@ -6,5 +6,14 @@ class Utilisateur{
     protected ?String $prenom;
     protected ?String $password;
     protected ?String $role;
+    public function __construct($id='',$email='',$nom='',$prenom='',$password='',$role="member")
+    {
+        $this->id=$id;
+        $this->email=$email;
+        $this->nom=$nom;
+        $this->prenom=$prenom;
+        $this->password=$password;
+        $this->role=$role;
+    }
 }
 ?>
