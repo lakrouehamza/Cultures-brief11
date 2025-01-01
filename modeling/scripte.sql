e20bb717c7e977e922f82416246e6741a4340ded
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-01 20:11:08 +0100

    création des tables Admin, Members, et Auteurs, ainsi que des tables pour les likes, les vues, les articles, et les catégories. Mise en place d'un trigger pour insérer automatiquement les données dans les tables Admin, Members, et Auteurs en fonction des rôles

--- modeling/scripte.sql
+++ modeling/scripte.sql
@@ -1,5 +1,11 @@
 create database cultures;
 use cultures;
+
+drop  table utilisateur;
+drop table admin;
+drop  table Members;
+drop table Auteur;
+
 create table Utilisateur (
     id int primary key auto_increment,
     nom varchar(50) not null,
@@ -8,3 +14,69 @@ create table Utilisateur (
     password varchar(100) not null,
     role  enum('membre','admin','auteur')
 );
+create table Admin (
+    id int primary key ,
+    constraint FK_admin  FOREIGN KEY(id) references Utilisateur(id) on delete cascade on update cascade 
+);
+create table Members (
+    id int primary key ,
+    constraint FK_member  FOREIGN KEY(id) references Utilisateur(id) on delete cascade on update cascade
+);
+create table Auteur (
+    id int primary key ,
+    constraint FK_auteur  FOREIGN KEY(id) references Utilisateur(id) on delete cascade on update cascade
+);
+create table Categorie (
+    id int primary key auto_increment ,
+    titre varchar(100) not null,
+    admin int not null,
+    constraint FK_adminCtgr  foreign  key (admin) references  Admin(id) on delete cascade on update cascade
+);
+create table Article (
+    id int premary key auto_increment ,
+    statut enum('confirme','annule','encours'),
+    auteur int not null,
+    titre varchar(100) not null,
+    contraire text  not null,
+    categor int not null,
+    constraint  FK_auteurArtcl  foreign key (auteur) references Auteur (id) on delete cascade on update cascade,
+    constraint FK_typeArticle foreign key (categor) references Categorie(id) on delete cascade on update cascade
+);
+create  table leslikes (
+    id int premary key auto_increment,
+    membre int not null,
+    article int not null,
+    constraint FK_ArticleLik foreign key (article) references Article(id) on delete cascade on update cascade,
+    constraint FK_AuteurLik foreign key (membre) references Members (id) on delete cascade on update cascade,
+ );
+ create  table lesviews(
+    id int premary key auto_increment,
+    article int not null,
+    constraint FK_Articlewies foreign key (article) references Article(id) on delete cascade on update cascade
+ ); 
+create table lescommits(
+    id int primary key auto_increment,
+    contraire varchar(100) not null,
+    auteur int not null,
+    reply  int ,
+    constraint FK_AuteurCommit foreign key (auteur) references Members (id) on delete cascade on update cascade,
+    constraint FK_Rply foreign key(reply) references lescommits(id) on delete cascade on update cascade
+);
+
+DELIMITER //
+create Trigger insetRole
+after insert 
+on Utilisateur
+for each row 
+begin 
+if new.role = 'admin' then 
+insert into Admin values (new.id);
+elseif new.role = 'auteur' then
+insert into Auteur values (new.id);
+else 
+insert into Members values (new.id);
+end if;
+end //
+DELIMITER ;
+
+
