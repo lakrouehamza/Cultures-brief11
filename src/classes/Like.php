8dacc706a8e69b01da48361d9dae41eeb7a15ace
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-01 23:49:27 +0100

    ajoute la class Like

---
+++ src/classes/Like.php
@@ -0,0 +1,29 @@
+<?php 
+class Like{
+    private ?int $id;
+    private ?int $member;
+    private ?int $article;
+    public function __construct()
+    {
+        
+    }
+    public function setId($id){
+        $this->id =$id;
+    }
+    public function setAember($member){
+        $this->member =$member;
+    }
+    public function setArticle($article){
+        $this->article =$article;
+    }
+    public function getId(){
+        $this->id ;
+    }
+    public function getAember(){
+        $this->member ;
+    }
+    public function getArticle(){
+        $this->article ;
+    }
+}
+?>
