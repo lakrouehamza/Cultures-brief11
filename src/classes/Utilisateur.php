a2fbb7e2aba17ec2f79e444eeebb8cb5371a1e4f
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-01 12:22:21 +0100

    ajoutez les getters et setters à la classe Utilisateur

--- src/classes/Utilisateur.php
+++ src/classes/Utilisateur.php
@@ -15,5 +15,41 @@ class Utilisateur{
         $this->password=$password;
         $this->role=$role;
     }
+    public function setId($id){
+        $this->id=$id;
+    }
+    public function setEmail($email){
+        $this->email=$email;
+    }
+    public function setNom($nom){
+        $this->nom=$nom;
+    }
+    public function setPrenom($prenom){
+        $this->prenom=$prenom;
+    }
+    public function setPassword($password){
+        $this->password=$password;
+    }
+    public function setRole($role){
+        $this->role=$role;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    public function getNom(){
+        return $this->nom;
+    }
+    public function getPrenom(){
+        return $this->prenom;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+    public function getRole(){
+        return $this->role;
+    }
 }
 ?>
