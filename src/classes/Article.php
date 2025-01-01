3e189e41b8396aa4f3e4090f82a8c578dcff8e93
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-01 23:19:24 +0100

    ajoutez les getters et setters à la classe Article

--- src/classes/Article.php
+++ src/classes/Article.php
@@ -15,5 +15,41 @@ public function __construct($id=0,$statut,$auter,$titre,$categor, $container)
     $this->categor=$categor;
     $this->container=$container;
 }
+public function setId($id){
+    $this->id =$id;
+}
+public function setStatut($statut){
+    $this->statut =$statut;
+}
+public function setAuter($auter){
+    $this->auter =$auter;
+}
+public function setTitre($titre){
+    $this->titre =$titre;
+}
+public function setCategor($categor){
+    $this->categor =$categor;
+}
+public function setContainer($container){
+    $this->container =$container;
+}
+public function getTitre(){
+    return $this->titre ;
+}
+public function getStatut(){
+    return $this->statut ;
+}
+public function getAuter(){
+    return $this->auter ;
+}
+public function getCategor(){
+    return $this->categor ;
+}
+public function getContainer(){
+    return $this->container ;
+}
+public function getId(){
+    return $this->id ;
+}
 }
 ?>
