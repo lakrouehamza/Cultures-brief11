af1718032cd87f34e6fb932a6afa72407629949b
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-02 10:42:37 +0100

    ajoute un méthode pou remplir un categorie

--- src/classes/Categorie.php
+++ src/classes/Categorie.php
@@ -25,5 +25,18 @@ class Categorie{
     public function getAdmin(){
         return $this->admin;
     }
+    public function remplir(){
+        $id =  $this->getId();
+        $conn = new Connect();
+        $stmt= $conn->getConnect()->prepare("select *  from  Categorie where id = :id");
+        $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+        $stmt->execute();
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $this->setTitre($row['titre']);
+            $this->setAdmin($row['admin']);
+            return true ;
+        }
+        return false;
+    }
 }
 ?>
