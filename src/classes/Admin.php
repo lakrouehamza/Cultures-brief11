64692aee8238c1cef97f02b6a1715b1a37d63a9b
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-02 09:54:33 +0100

    ajoute un méthode pour supprimer un categorie

--- src/classes/Admin.php
+++ src/classes/Admin.php
@@ -8,12 +8,27 @@ public function crretionCategorie($categorie){
         $stmt = $connect->getConnect()->prepare("insert into Categorie(titre,admin) values (:titre,:admin)");
         $titre = $categorie->getTitre();
         $admin = $categorie->getAdmin();
-        $stmt->bindParam(":titre",$titre);
-        $stmt->bindParam(":admin",$admin);
+        $stmt->bindParam(":titre",$titre,PDO::PARAM_STR);
+        $stmt->bindParam(":admin",$admin,PDO::PARRAM_INT);
         return $stmt->execute();
     }
     return false;
 }
+
+public function deleteCategorie($categorie){
+    if($categorie instanceof Categorie ){
+        $conn =  new Connect();
+        $stmt = $conn->getConnect()->prepare("delete from Categorie where id = :id and admin = :admin");
+        $admin = $this->getId();
+        $id =  $categorie->getId();
+        $stmt->bindParam("id",$id,PDO::PARAM_INT);
+        $stmt->bindParam(":admin",$admin,PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+    return false;
 }
+
+
+};
 
 ?>
